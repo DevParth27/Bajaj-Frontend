31585d6093db48436ef14eefa5caff3869c8f965
devparth27 <parth.upadhye.4@gmail.com>
2025-11-08 15:49:28 +0530

    feat(api): add authorization header to API requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,18 @@
 import axios from 'axios';
 
 const API_URL = 'http://127.0.0.1:8000/api/v1/hackrx/run';
+const TOKEN = '433c9562217435ac71d779508405bfa9b20d0f58ff2aeb482c16c0e251f9f85f';
 
 export const getAnswers = (documents, questions) => {
-  return axios.post(API_URL, { documents, questions });
+  return axios.post(
+    API_URL,
+    { documents, questions },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${TOKEN}`,
+      },
+      withCredentials: false,
+    }
+  );
 };
